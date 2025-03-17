9e9d12747a528a96d149de686ea5e1562d513ef9
asim58454 <asim.58454@iqra.edu.pk>
2025-03-17 22:59:08 +0500

    Add

--- config/db.js
+++ config/db.js
@@ -3,14 +3,11 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("MongoDB Connected");
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("✅ MongoDB Connected Successfully");
   } catch (err) {
-    console.error("DB Connection Error:", err.message);
-    process.exit(1);
+    console.error("❌ MongoDB Connection Error:", err.message);
+    process.exit(1); // Exit process with failure
   }
 };
 
